7cb687e9870a4d1d797dc9e08b35fc361d9bcae9
cw-seungkwon <20020903@coway.com>
2023-02-21 02:32:51 +0900

    feat: 지표를 분리하는 기능 추가 #1

--- src/main/kotlin/me/yevgnenll/stock/dto/embed/Quote.kt
+++ src/main/kotlin/me/yevgnenll/stock/dto/embed/Quote.kt
@@ -1,9 +1,59 @@
 package me.yevgnenll.stock.dto.embed
 
+import org.slf4j.LoggerFactory
+
 data class Quote(
-    val open: ArrayList<Double> = arrayListOf(),
-    val high: ArrayList<Double> = arrayListOf(),
-    val close: ArrayList<Double> = arrayListOf(),
-    val low: ArrayList<Double> = arrayListOf(),
-    val volume: ArrayList<Double> = arrayListOf()
-)
+    val open: List<Double> = listOf(),
+    val high: List<Double> = listOf(),
+    val close: List<Double> = listOf(),
+    val low: List<Double> = listOf(),
+    val volume: List<Double> = listOf()
+) {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(javaClass)
+    }
+
+    internal fun xorAllCollectionSize(timestampSize: Int): Int =
+        open.size xor high.size xor close.size xor low.size xor volume.size xor timestampSize
+
+    private fun validate(timestampSize: Int) {
+        val xorResult = xorAllCollectionSize(timestampSize)
+
+        if (xorResult != 0) {
+            logger.error("Stock Quete data length is INVALID")
+            throw IllegalArgumentException("Data is INVALID")
+        }
+    }
+
+    fun separate(timestampSize: Int): List<QuoteItem> {
+        validate(timestampSize)
+
+        return volume.indices.map {
+            QuoteItem(it, open, high, close, low, volume)
+        }
+    }
+}
+
+data class QuoteItem(
+    val open: Double,
+    val high: Double,
+    val close: Double,
+    val low: Double,
+    val volume: Double,
+) {
+    constructor(
+        index: Int,
+        open: List<Double>,
+        high: List<Double>,
+        close: List<Double>,
+        low: List<Double>,
+        volume: List<Double>,
+    ) : this(
+        open = open[index],
+        high = high[index],
+        close = close[index],
+        low = low[index],
+        volume = volume[index],
+    )
+}
---
+++ src/test/kotlin/me/yevgnenll/stock/dto/embed/QuoteTest.kt
@@ -0,0 +1,35 @@
+package me.yevgnenll.stock.dto.embed
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class QuoteTest {
+
+    @Test
+    fun `Quote 내부 collection들의 사이즈가 timestamp 사이즈와 다르면 에러를 반환한다`() {
+
+        assertThrows(IllegalArgumentException::class.java) {
+            Quote(
+                open = listOf(10.0, 10.0),
+                close = listOf(11.0, 11.0, 11.0),
+                high = listOf(12.0, 12.0, 12.0),
+                low = listOf(13.0, 13.0, 13.0),
+                volume = listOf(14.0, 14.0, 14.0)
+            ).separate(3)
+        }
+    }
+
+    @Test
+    fun `Quote 내부 collection들의 사이즈가 timestamp와 일치하면 0을 반환한다`() {
+        Quote(
+            open = listOf(10.0, 10.0, 10.0),
+            close = listOf(11.0, 11.0, 11.0),
+            high = listOf(12.0, 12.0, 12.0),
+            low = listOf(13.0, 13.0, 13.0),
+            volume = listOf(14.0, 14.0, 14.0)
+        ).xorAllCollectionSize(3).let {
+            assertThat(it).isEqualTo(0)
+        }
+    }
+}
