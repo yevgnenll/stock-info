183bf42fa94efa9d37d8da426b61fc3151a24c7e
yevgnenll <yevgnenll@gmail.com>
2023-02-23 03:15:44 +0900

    chore: 필요 없는 쿼리 제거 #4
    jpa auto ddl 사용함

--- mysql/init/db/stocks.sql
+++ mysql/init/db/stocks.sql
@@ -1,17 +1,2 @@
 CREATE SCHEMA IF NOT EXISTS `stocks`;
 USE `stocks`;
-
-create table stocks.stock (
-    id         bigint auto_increment
-        primary key,
-    close      int         not null,
-    created_at datetime(6) null,
-    high       int         not null,
-    low        int         not null,
-    open       int         not null,
-    timestamp  date        null,
-    updated_at datetime(6) null,
-    volume     int         not null
-);
-
-
