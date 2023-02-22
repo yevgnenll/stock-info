0a9b846a1a1513008566cb95f102d00502b5e198
cw-seungkwon <20020903@coway.com>
2023-02-22 21:58:58 +0900

    chore: file 한번만 읽기, 함수 이름 변경 #1

--- src/test/kotlin/me/yevgnenll/stock/dto/StockInfoDtoTest.kt
+++ src/test/kotlin/me/yevgnenll/stock/dto/StockInfoDtoTest.kt
@@ -7,23 +7,26 @@ import org.springframework.util.ResourceUtils
 
 class StockInfoDtoTest {
 
-    private val objectMapper = ObjectMapper()
 
-    private fun readStockInfoDto(): StockInfoDto =
-        ResourceUtils.getFile("classpath:2023Feb20.json").let {
+    private val stockInfo = readStockInfoDto()
+
+    private fun readStockInfoDto(): StockInfoDto {
+        val objectMapper = ObjectMapper()
+        return ResourceUtils.getFile("classpath:2023Feb20.json").let {
             objectMapper.readValue(it.readText(Charsets.UTF_8), StockInfoDto::class.java)
         }
+    }
 
     @Test
-    fun `API 날짜들이 올바르게 추출되는지 검증한다`() {
-        readStockInfoDto().exportStockEntity().let {
+    fun `API timestamp 5개가 존재하는지 검증한다`() {
+        stockInfo.exportStockEntity().let {
             assertThat(it.size).isEqualTo(5)
         }
     }
 
     @Test
     fun `API 5일전의 결과는 open-63600 volume-70606 high-63900 low-63200 close-63200 이다`() {
-        readStockInfoDto().exportStockEntity()[0].let {
+        stockInfo.exportStockEntity().first().let {
             assertThat(it.open).isEqualTo(63600.0)
             assertThat(it.close).isEqualTo(63200.0)
             assertThat(it.high).isEqualTo(63900.0)
