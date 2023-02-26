4b359c23607a48dcf69c64d6bcce0f4736e90c37
yevgnenll <yevgnenll@gmail.com>
2023-02-27 01:27:20 +0900

    feat: interval 은 1일로 강제 #17

--- src/main/kotlin/me/yevgnenll/stock/controller/RequestParamDto.kt
+++ src/main/kotlin/me/yevgnenll/stock/controller/RequestParamDto.kt
@@ -16,9 +16,7 @@ data class RequestParamDto(
         )
 
         private val validInterval = setOf(
-            "1m", "2m", "5m", "15m", "30m",
-            "60m", "90m", "1h", "1d", "5d",
-            "1wk", "1mo", "3mo"
+            "1d",
         )
     }
 
